5cda38a0128c2754d88d0984271f4c6f211b21a4
ChangJoo Park <pcjpcj2@gmail.com>
2017-08-19 15:59:24 +0900

    Randomize participants, winners

--- app/home/home-view-model.js
+++ app/home/home-view-model.js
@@ -1,11 +1,18 @@
 var observableModule = require("data/observable");
 
 function HomeViewModel() {
+  const participantCount = getRandomInt(10, 200) || 100;
+  const winnerCount = getRandomInt(1, participantCount * 0.1) || 10;
+
   const viewModel = observableModule.fromObject({
-    participant: "100",
-    winner: "10"
+    participant: participantCount.toString(),
+    winner: winnerCount.toString()
   });
   return viewModel;
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 module.exports = HomeViewModel;
